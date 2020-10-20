48af34cf1f15190ed2048651cecd75442f523496
fixiabis <fixiabis@gmail.com>
2020-10-20 14:31:47 +0800

    feat: add makeSharedState

---
+++ src/makeSharedState.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import { Subject } from 'rxjs';
+
+const makeSharedState = <State>(initialState: State) => {
+  const sharedState = new Subject<State>();
+  const setSharedState = sharedState.next.bind(sharedState);
+
+  const useSharedState = () => {
+    const [state, setState] = useState<State>(initialState);
+
+    useEffect(() => {
+      const subscriber = sharedState.subscribe(setState);
+      return () => subscriber.unsubscribe();
+    }, []);
+
+    return [state, setSharedState] as [State, typeof setSharedState];
+  };
+
+  return useSharedState;
+};
+
+export default makeSharedState;
