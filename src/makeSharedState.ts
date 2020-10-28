b36e43f48937695ff5f181f1ecd46f4f69eaa7f3
fixiabis <fixiabis@gmail.com>
2020-10-28 09:09:39 +0000

    fix: solve initializer & updater problem

--- src/makeSharedState.ts
+++ src/makeSharedState.ts
@@ -1,15 +1,26 @@
 import { useState, useEffect } from 'react';
 import { Subject } from 'rxjs';
 
-const makeSharedState = <State>(initialState: State) => {
-  const sharedState = new Subject<State>();
-  const setSharedState = sharedState.next.bind(sharedState);
+const makeSharedState = <State>(initialStateOrInitializer: State | (() => State)) => {
+  const sharedStateSubject = new Subject<State>();
+
+  let sharedState: State = typeof initialStateOrInitializer === 'function'
+    ? (initialStateOrInitializer as () => State)()
+    : initialStateOrInitializer;
+
+  const setSharedState = (stateOrUpdater: State | ((prevState: State) => State)) => {
+    const state = typeof stateOrUpdater === 'function'
+      ? (stateOrUpdater as (prevState: State) => State)(sharedState)
+      : stateOrUpdater;
+
+    sharedStateSubject.next(sharedState = state);
+  };
 
   const useSharedState = () => {
-    const [state, setState] = useState<State>(initialState);
+    const [state, setState] = useState<State>(sharedState);
 
     useEffect(() => {
-      const subscriber = sharedState.subscribe(setState);
+      const subscriber = sharedStateSubject.subscribe(setState);
       return subscriber.unsubscribe.bind(subscriber);
     }, []);
 
